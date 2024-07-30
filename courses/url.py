7d53dd3f4c34375bae1f7642962cab82e5eee306
Fatmanur Özçetin <93328444+Nurrozcetin@users.noreply.github.com>
2024-07-30 17:42:35 +0300

    add static and global files

--- courses/url.py
+++ courses/url.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index ),
-    path('list/', views.courses ),
     path('<course_name>', views.details),
-    path('course/<int:category_id>', views.getCoursesByCategoryId),
-    path('course/<str:category_name>', views.getCoursesByCategory),
+    path('category/<int:category_id>', views.getCoursesByCategoryId),
+    path('category/<str:category_name>', views.getCoursesByCategory, name='courses_by_category')
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
@@ -8,20 +9,54 @@ data = {
     "mobile":"get courses by mobile"
 }
 
+db = {
+    "courses": [
+        {
+            "title": "javascript course",
+            "desc": "javascript course desc",
+            "imageUrl": "webb.jfif",
+            "slug": "javascript-course",
+            "date": date(2022,10,15), 
+            "isActive": True,
+            "isUpdated": True
+        },
+        {
+            "title": "python course",
+            "desc": "python course desc",
+            "imageUrl": "pythonn.png",
+            "slug": "python-course",
+            "date": date(2022,10,15),
+            "isActive": True,
+            "isUpdated": False
+        },
+        {
+            "title": "kotlin course",
+            "desc": "kotlin course desc",
+            "imageUrl": "kotlin.png",
+            "slug": "kotlin-course",
+            "date": date(2022,10,15),
+            "isActive": False,
+            "isUpdated": False
+        }
+    ],
+    "categories": [
+        { "id": 1, "name": "programming", "slug": "programming"},
+        { "id": 2, "name": "web development", "slug": "web development"},
+        { "id": 3, "name": "mobile development", "slug": "mobile development"}
+    ]
+}
 def index(request):
-    return render(request, 'courses/index.html')
+    courses = []
+    categories = db["categories"]
 
-def courses(request):
-    list_items = ""
-    category_list = list(data.keys())
+    for course in db["courses"]:
+        if course["isActive"] == True:
+            courses.append(course)
 
-    for category in category_list:
-        redirect_url = reverse('courses by category', args=[category])
-        list_items += f"<li><a> href ='{redirect_url}'>{category}</a></li>"
-    
-    html = f"<h1>courses list</h1><ul>{list_items}</ul>"
-
-    return HttpResponse(html)
+    return render(request, 'courses/index.html', {
+        'categories': categories,
+        'courses': courses
+    })
 
 def details(request, course_name):
     return HttpResponse(f'{course_name} detail course')
@@ -30,16 +65,20 @@ def details(request, course_name):
 def getCoursesByCategory(request, category_name):
     try:
         category_text = data[category_name]
-        return HttpResponse(category_text)
+        return render(request, 'courses/courses.html', {
+            'category': category_name,
+            'category_text': category_text
+        })
     except:
         return HttpResponseNotFound("<h1>Wrong category</h1>")
 
 def getCoursesByCategoryId(request, category_id):
     category_lists = list(data.keys())
-    if(category_id >= len(category_lists)):
+    if(category_id > len(category_lists)):
         return HttpResponseNotFound("wrong category id ")
     
     category_name = category_lists[category_id - 1]
+
     redirect_url = reverse('courses by category', args=[category_name])
     return redirect(redirect_url)
 
