5105d354b2ced375481eb661a50200eea21a034c
Fatmanur Özçetin <93328444+Nurrozcetin@users.noreply.github.com>
2024-08-05 10:53:53 +0300

    dinamic slider with models

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Slider
 from .models import Category
 
 @admin.register(Course)
@@ -23,3 +23,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
     def course_count(self, obj):
         return obj.course_set.count()
+    
+admin.site.register(Slider)
--- courses/views.py
+++ courses/views.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 from courses.forms import CourseCreateForm, UploadForm
-from .models import Course, UploadModel
+from .models import Course, Slider, UploadModel
 from .models import Category
 from django.contrib.auth.decorators import login_required, user_passes_test
 
@@ -10,10 +10,12 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 def index(request):
     courses = Course.objects.filter(isActive = 1, isHome=1)
     categories = Category.objects.all()
+    sliders = Slider.objects.filter(is_active=True)
 
     return render(request, 'courses/index.html', {
         'categories': categories,
-        'courses': courses
+        'courses': courses,
+        'sliders': sliders
     })
 
 def search(request):
