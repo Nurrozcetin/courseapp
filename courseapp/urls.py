70bf8da099d4caf1e72eb44f9de3b9d6c21c77cb
Fatmanur Özçetin <93328444+Nurrozcetin@users.noreply.github.com>
2024-08-05 11:57:27 +0300

    venv, serving static files and getenv

--- courseapp/urls.py
+++ courseapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
